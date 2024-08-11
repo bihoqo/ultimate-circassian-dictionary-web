251708b4a63afb6eaf9d261e485ae72981990de8
ArturMarschenkulov <ArturMarschals@web.de>
2024-08-12 00:15:33 +0200

    refactor sendEmail code

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -1,27 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import nodemailer from "nodemailer";
+import nodemailer, { Transporter } from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
 
+//TODO(artur): Implement sending email to the email which was provided, so that the user knows what they sent.
+//TODO(artur): Probably, should implement some mechanism to prevent spam, but right now we are probably
+//             too small to be targeted by spammers.
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    const { email, subject, message } = req.body;
-
-    // Create a transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-      host: "smtp.example.com", // Replace with your mail server details
+    //TODO(artur): Right now `userSendemail` is not used. It should be used to identify to us where it is from.
+    const { userSendemail, subject, message } = req.body;
+    // port 587: TLS
+    // port 465: SSL
+    const transporter: Transporter<SMTPTransport.SentMessageInfo> = nodemailer.createTransport({
+      host: "smtp.gmail.com", // Replace with your mail server details
       port: 587,
       secure: false, // true for 465, false for other ports
       auth: {
-        user: process.env.EMAIL_USER, // Your email id
-        pass: process.env.EMAIL_PASS, // Your password
+        user: "learncircassian@gmail.com",
+        pass: process.env.EMAIL_PASS,
       },
     });
 
-    // Send mail with defined transport object
-    const info = await transporter.sendMail({
-      from: email, // Sender address
-      to: "learncircassian@gmail.com", // List of receivers
-      subject: subject, // Subject line
-      text: message, // Plain text body
+    const info: SMTPTransport.SentMessageInfo = await transporter.sendMail({
+      from: "learncircassian@gmail.com",
+      to: "learncircassian@gmail.com",
+      subject: subject,
+      text: message,
     });
 
     res.status(200).json({ status: "Email Sent", info });
