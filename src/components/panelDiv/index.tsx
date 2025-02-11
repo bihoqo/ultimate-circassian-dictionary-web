f92bf26aa088fc316b781257a454fe2022c98cd9
Nart <113383731+nartna123@users.noreply.github.com>
2025-02-11 14:57:52 +0200

    .

--- src/components/panelDiv/index.tsx
+++ src/components/panelDiv/index.tsx
@@ -1,3 +1,19 @@
-export default function PanelDiv({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-col gap-6">{children}</div>;
+import { FaCaretRight } from "react-icons/fa6";
+
+export default function PanelDiv({
+  children,
+  audioPath,
+}: {
+  children: React.ReactNode;
+  audioPath?: string;
+}) {
+  return (
+    <div className="flex flex-col gap-6 w-full">
+      <button className="flex flex-row gap-1 justify-center items-center self-end hover:opacity-80">
+        <FaCaretRight color="#39a9b0" size={20} className="mt-0.5" />
+        <p className="font-black text-[#39a9b0] text-xl">Play Audio</p>
+      </button>
+      <div className="flex flex-row justify-center items-center">{children}</div>
+    </div>
+  );
 }
