7e564afba14eb214b4a19d17d3e0ab8ccf33a082
Artur Marschenkulov <ArturMarschals@web.de>
2025-05-21 22:33:03 +0200

    Update to remove linting errors (#161)
    * limit unnecessary exports for internal utilities and consistency
    
    - Changed `toastUtil` from `export` to internal-only usage, as it's not intended for external access
    - Made `ToastContent` exportable to allow reuse in other components
    - Removed default export from `useModal`, favoring named usage for clearer imports and better tree-shaking
    - Updated `getServerSideProps` in `debug` and `dictionary` pages to non-exported constants to align with typical Next.js usage where only page components are default-exported
    
    * add jest for testing
    
    * add dummy test
    
    * add jest config
    
    * add ts-jest to yarn
    
    * upgrade packages

---
+++ src/__test__/test_foo.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "@jest/globals";
+
+describe("foo", () => {
+  it("foo", () => {
+    const input = "foo";
+    const expectedOutput = "foo";
+    expect(input).toBe(expectedOutput);
+  });
+});
